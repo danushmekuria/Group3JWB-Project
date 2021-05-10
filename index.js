f53fff88ab332dda0c7f64f5bee459215783826e
alebachew450 <alexokeb@gmail.com>
2021-05-10 08:54:15 -0400

    change form 05/10/2021

---
+++ index.js
@@ -0,0 +1,22 @@
+const taskName = document.querySelector('#tName');
+const description = document.querySelector("#tDescription");
+const assinged = document.querySelector('#assignTo');
+const status = document.querySelector('#Status');
+const dueDate = document.querySelector('#dueDate');
+const button = document.querySelector('#btnSubmit');
+// const name = newTaskNameInput.value;
+
+function validFormFieldInput(data) {
+    
+ }
+
+FormData.addEventListener('add task', (e) => {
+    let message = []
+    if (name.value === '' || name value == null) {
+        messages.push('Name is required')
+    }
+}
+) => {
+
+})
+console.log("name");
